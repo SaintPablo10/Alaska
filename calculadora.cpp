f7495772c019402542c9b6fa5c42f376b3a79102
Morales57 <wilfrido.morales.torres@utelvt.edu.ec>
2025-03-25 03:21:40 +0000

    XDD

--- calculadora.cpp
+++ calculadora.cpp
@@ -7,9 +7,9 @@ int main()
 {
 	float CSNR_x,CSNR_y,CSNR_s,CSNR_m,CSNR_d,CSNR_r;
 	//Ingreso de datos
-	cout<<"Ingrese en valor de SSSS_x=:";
+	cout<<"Ingrese en valor de SSSS_x=:"zzzz;
 	cin>>CSNR_x;
-	cout<<"Ingrese en valor de SSSS_y=:";
+	cout<<"Ingrese en valor de SSSzzzzzzS_y=:";
 	//Operaciones 
 	cin>>CSNR_y;
 	CSNR_s=CSNR_x+CSNR_y;
